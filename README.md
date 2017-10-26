07988071a1637f20994043f55982815b87ee8a37
Kuei-chun Chen <simagix@gmail.com>
2017-10-25 20:36:49 -0500

    add more fields to robots
    Signed-off-by: Kuei-chun Chen <simagix@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # MongoDB Sanity Check
+A simple sanity check tool written in GO.  Execute the binary without installing application (such as Java, Python, and/or other 3pp software)
+## Download and Execute
+### On Linux
+- Download [mcheck-linux-x64](https://github.com/simagix/mongodb-sanity-check/blob/master/build/mcheck-linux-x64)
+- Run as, for example
+```
+$ mcheck-linux-x64 mongodb://localhost:27017
+```
+### On OS X
+- Download [mcheck-osx-x64](https://github.com/simagix/mongodb-sanity-check/blob/master/build/mcheck-osx-x64)
+- Run as, for example
+```
+$ mcheck-osx-x64 mongodb://localhost:27017
+```
+### On Windows
+- Download [mcheck-win-x64](https://github.com/simagix/mongodb-sanity-check/blob/master/build/mcheck-win-x64)
+- Run as, for example
+```
+> mcheck-win-x64 mongodb://localhost:27017
+```
+## Develop, Build, and Execute
+```
+$ go run mcheck.go
+$ go build mcheck.go
+$ ./mcheck
+```
+
+### Usage
+```
+$ build/mcheck-osx-x64 -h
+Usage of build/mcheck-osx-x64:
+  -mongoURI string
+    	MongoDB URI (default "mongodb://localhost")
+  -t int
+    	number of threads (default 1)
+  -total int
+    	total ops in a batch (default 1000)
+```
+
+## Cross Compile
+Run the *build.sh* script to create binaries for Linux, Windows, and OS X.  The *build.sh* script has the following builds.
+```
+$ mkdir build
+$ env GOOS=linux GOARCH=amd64 go build -o build/mcheck-linux-x64 mcheck.go
+$ env GOOS=windows GOARCH=amd64 go build -o build/mcheck-win-x64 mcheck.go
+$ env GOOS=darwin GOARCH=amd64 go build -o build/mcheck-osx-x64 mcheck.go
+```
---
+++ mcheck.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "log"
+    "strconv"
+    "time"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+)
+
+type Statistics struct {
+    Tasked int
+    Battery int
+    Maint   bool
+}
+
+type Robot struct {
+    Name string
+    Stats Statistics
+    Updated time.Time
+}
+
+func connectMongo(mongoURI string, total int, thread int) {
+    fmt.Println(mongoURI)
+    session, err := mgo.Dial("localhost")
+    if err != nil {
+            panic(err)
+    }
+    defer session.Close()
+
+    // Optional. Switch the session to a monotonic behavior.
+    session.SetMode(mgo.Monotonic, true)
+
+    c := session.DB("mcheck").C("robots")
+    err = c.Insert(&Robot{"Robot", Statistics{Tasked: 0, Battery: 95, Maint: false}, time.Now()})
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    bnum := thread * 100000
+    for {
+	    start := time.Now()
+	    for i := bnum; i < (bnum+total); i++ {
+            robot := "Robot-" + strconv.Itoa(i)
+            err = c.Insert(&Robot{robot, Statistics{Tasked: 0, Battery: 95, Maint: false}, time.Now()})
+	        if err != nil {
+	            log.Fatal(err)
+	        }
+	    }
+	    elapsed := time.Since(start)
+	    log.Printf("%d inserts took %s", total, elapsed)
+
+        result := Robot{}
+	    start = time.Now()
+	    for i := bnum; i < (bnum+total); i++ {
+            robot := "Robot-" + strconv.Itoa(i)
+            err = c.Find(bson.M{"name": robot}).One(&result)
+	        if err != nil {
+	            log.Fatal(err)
+	        }
+	    }
+	    elapsed = time.Since(start)
+	    log.Printf("%d finds took %s", total, elapsed)
+
+	    start = time.Now()
+	    for i := bnum; i < (bnum+total); i++ {
+            robot := "Robot-" + strconv.Itoa(i)
+            err = c.Find(bson.M{"name": robot}).One(&result)
+	        if err != nil {
+	            log.Fatal(err)
+	        }
+            change := bson.M{"$inc": bson.M{"stats.tasked": 1}}
+            err = c.Update(bson.M{"name": robot}, change)
+	        if err != nil {
+	            log.Fatal(err)
+	        }
+	    }
+	    elapsed = time.Since(start)
+	    log.Printf("%d updates took %s", total, elapsed)
+        fmt.Println("")
+
+        bnum = bnum + total
+        time.Sleep(time.Millisecond * 100)
+    }
+
+    result := Robot{}
+    err = c.Find(bson.M{"name": "Robot"}).One(&result)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("Updated:", result.Updated)
+    fmt.Println("=== END ===")
+}
+
+func main() {
+    total := flag.Int("total", 1000, "total ops in a batch")
+    threads := flag.Int("t", 1, "number of threads")
+    mongoURI := flag.String("mongoURI", "mongodb://localhost", "MongoDB URI")
+    flag.Parse()
+
+    fmt.Println("threads:", *threads)
+    fmt.Println("MongoDB URI:", *mongoURI)
+
+    for i := 0; i < *threads; i++ {
+        go connectMongo(*mongoURI, *total, i)
+    }
+
+    var input string
+    fmt.Scanln(&input)
+}
+
