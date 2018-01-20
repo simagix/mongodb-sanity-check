793fc9f7ff03b32e78fea89d3dfdf74166d9c9a6
Kuei-chun Chen <simagix@gmail.com>
2018-01-20 18:55:24 -0500

    Diff server and client certs

--- certificates/create_certs.sh
+++ certificates/create_certs.sh
@@ -31,10 +31,8 @@ prompt = no
 distinguished_name = dn
 default_md = x509
 x509_extensions = v3_req
-
 [dn]
 $DN
-
 [v3_req]
 subjectAltName = @alt_names
 subjectKeyIdentifier = hash
@@ -53,25 +51,13 @@ prompt = no
 distinguished_name = dn
 default_md = x509
 req_extensions = v3_req
-
 [dn]
 $DN
-
 [v3_req]
 EOF
 
-read -r -d '' EXTDATA <<-EOF
-basicConstraints = CA:FALSE
-keyUsage = nonRepudiation, digitalSignature, keyEncipherment
-subjectAltName = @alt_names
-
-[alt_names]
-DNS.1=localhost
-DNS.2=$(hostname -f)
-EOF
-
 # CA certificates
-echo "Creating server certificate and key file: ca.crt and ca.key"
+# echo "Creating server certificate and key file: ca.crt and ca.key"
 openssl req -nodes -x509 -days 365 -newkey rsa:2048 -keyout ca.key -out ca.crt -config <(
 cat <<-EOF
 $CADATA
@@ -87,14 +73,18 @@ EOF
 
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -days 365 -out server.crt -extfile <(
 cat <<-EOF
-$EXTDATA
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1=localhost
+DNS.2=$(hostname -f)
 EOF
 )
-echo "Creating server PEM file: cat server.key server.crt > server.pem"
 cat server.key server.crt > server.pem
 
 # Client certificates
-echo "Creating client certificate and key file: client.crt and client.key"
+# echo "Creating client certificate and key file: client.crt and client.key"
 openssl req -nodes -newkey rsa:2048 -keyout client.key -out client.csr -config <(
 cat <<-EOF
 $PEMDATA
@@ -102,10 +92,10 @@ EOF
 )
 openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAserial ca.srl -days 365 -out client.crt -extfile <(
 cat <<-EOF
-$EXTDATA
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
 EOF
 )
-echo "Creating client PEM file: cat client.key client.crt > client.pem"
 cat client.key client.crt > client.pem
 
 ls -l $TMP/certs/ca.crt $TMP/certs/server.pem $TMP/certs/client.pem
